a102e4b8123e68f3f427bc58de649a730bd76e7c
Wilbur-Django <dwxiao2019@gmail.com>
2019-11-03 15:40:45 +0800

    add and delete pbi

--- backtrack/backtrack/views.py
+++ backtrack/backtrack/views.py
@@ -20,6 +20,10 @@ def addPBI(request, project_name):
                 project=project
             )
             new_pbi.product_backlog = pb
+            if new_pbi.storypoints:
+                pb.total_story_points += new_pbi.storypoints
+                pb.remaining_story_points += new_pbi.storypoints
+                pb.save()
             new_pbi.save()
             return HttpResponse("pbi saved")
     else:
@@ -42,3 +46,41 @@ def createProject(request):
             "backtrack/create_project.html",
             {"form": form}
         )
+
+def deletePBI(request, project_name):
+    project = get_object_or_404(models.Project, pk=project_name)
+    if request.method == 'GET':
+        return render(request, 'backtrack/delete_pbi.html', \
+            {'project': project})
+    try:
+        selected_pbis = []
+        pb = project.productbacklog
+        for pbi in request.POST.getlist('pbi'):
+            selected_pbis.append(
+                pb.pbi_set.get(pk=pbi)
+            )
+    except (KeyError, models.PBI.DoesNotExist):
+        return render(
+            request,
+            'backtrack/delete_pbi.html',
+            {
+                'project': project,
+                'error_message': "You didn't select a pbi"
+            }
+        )
+    else:
+        for selected_pbi in selected_pbis:
+            if selected_pbi.status != models.PBI.NOTSTARTED:
+                return render(request,'backtrack/delete_pbi.html',
+                    {'project': project,
+                    'error_message':\
+                        "You cannot delete a started pbi"
+                    }
+                )
+            if selected_pbi.storypoints:
+                pb.total_story_points -= selected_pbi.storypoints
+                pb.remaining_story_points -= \
+                    selected_pbi.storypoints
+            selected_pbi.delete()
+            pb.save()
+        return HttpResponse('deleted')
